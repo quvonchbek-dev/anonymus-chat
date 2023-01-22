7451c2d9e53fc0e284c15281fb988732ba30e899
Quvonchbek <quvonchbek.dev@gmail.com>
2023-01-22 13:50:20 +0500

    4

--- anonymus_chat/urls.py
+++ anonymus_chat/urls.py
@@ -1,11 +1,13 @@
+
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-from chat.views import index
+from chat.views import index, message_delete
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("ckeditor", include("ckeditor_uploader.urls")),
-    path("", index)
+    path("", index),
+    path(r'^delete/(?P<pk>[0-9]+)/$', message_delete, name='message_delete')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- chat/views.py
+++ chat/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Message
 from .forms import MessageForm
 from django.http import HttpResponseRedirect
@@ -16,3 +16,12 @@ def index(request):
         form = MessageForm()
     data = Message.objects.all().order_by('created_at')
     return render(request, 'index.html',{'form':form, "data": data})
+
+def message_delete(request, pk):
+    message = get_object_or_404(Message, pk=pk)  # Get your current message
+
+    if request.method == 'POST':         # If method is POST,
+        message.delete()                     # delete the message.
+        return redirect('/')             # Finally, redirect to the homepage.
+
+    return render(request, 'index.html', {'message': message})
