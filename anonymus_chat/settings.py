8e8c15bd4027c0da338abc3946428035ed824733
Quvonchbek <quvonchbek.dev@gmail.com>
2023-01-25 22:30:22 +0500

    LMS

--- anonymus_chat/settings.py
+++ anonymus_chat/settings.py
@@ -1,16 +1,9 @@
 import os
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "j)k4_c#&ue8e(e7^9+j)_x4vc5y)z+#tlai$sfzaje=gbcw1ma"
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
